b3009c24f34c55ac352d41f97633c1c457af37a2
Jackson Viana <jviana116@gmail.com>
2022-08-20 20:21:31 -0300

    feat: criando novos componentes e adicionando css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import FullFooter from './components/FullFooter';
+import BodyInicialPage from './pages/BodyInicialPage';
 import InicialPage from './pages/inicialPage';
 import './styles/app.css';
 
@@ -8,9 +10,11 @@ function App() {
       <div className="main">
         <InicialPage />
       </div>
+      <BodyInicialPage />
       <div>
         a
       </div>
+      <FullFooter />
     </div>
   );
 }
---
+++ src/components/FullFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/fullFooter.css';
+
+function FullFooter() {
+  return (
+    <footer className="footer-inicialpage">
+      <p>bad</p>
+    </footer>
+  );
+}
+
+export default FullFooter;
